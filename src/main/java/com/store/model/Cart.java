765d0cf5b675dcdace1e0d90a40ad2de383251f1
usb777 <dzsamoila@gmail.com>
2021-01-03 13:46:36 -0500

    Cart and Order models created

---
+++ src/main/java/com/store/model/Cart.java
@@ -0,0 +1,129 @@
+package com.store.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class Cart 
+{
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@Column(name = "cart_id")	
+	int cart_id;	
+		
+	@Column
+	int user_id;
+	
+	@Column
+	int product_id;
+	
+	@Column
+	Date date_added;
+	
+	@Column
+	String email;
+	
+	@Column
+	int quantity;
+
+	public Cart() 
+	{
+		super();
+	}	
+		
+	
+	public Cart(int cart_id, int user_id, int product_id, Date date_added, String email, int quantity) 
+	{
+		super();
+		this.cart_id = cart_id;
+		this.user_id = user_id;
+		this.product_id = product_id;
+		this.date_added = date_added;
+		this.email = email;
+		this.quantity = quantity;
+	}
+
+
+	public int getCart_id() {
+		return cart_id;
+	}
+
+
+	public void setCart_id(int cart_id) {
+		this.cart_id = cart_id;
+	}
+
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+
+	public Date getDate_added() {
+		return date_added;
+	}
+
+
+	public void setDate_added(Date date_added) {
+		this.date_added = date_added;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Cart [cart_id=" + cart_id + ", user_id=" + user_id + ", product_id=" + product_id + ", date_added="
+				+ date_added + ", email=" + email + ", quantity=" + quantity + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/store/model/Order.java
@@ -0,0 +1,100 @@
+package com.store.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Order
+{
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@Column(name = "order_id")
+	int order_id;
+	
+	@Column
+	int user_id;
+	
+	@Column
+	int product_id;
+	
+	@Column
+	Date order_date;
+	
+	@Column
+	int quantity;
+	
+	public Order() 
+	{
+		super();
+		
+	}
+
+	public Order(int order_id, int user_id, int product_id, Date order_date, int quantity)
+	{
+		super();
+		this.order_id = order_id;
+		this.user_id = user_id;
+		this.product_id = product_id;
+		this.order_date = order_date;
+		this.quantity = quantity;
+	}
+
+	public int getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public int getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+
+	public Date getOrder_date() {
+		return order_date;
+	}
+
+	public void setOrder_date(Date order_date) {
+		this.order_date = order_date;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "Order [order_id=" + order_id + ", user_id=" + user_id + ", product_id=" + product_id + ", order_date="+ order_date + ", quantity=" + quantity + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
